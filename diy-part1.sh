7b019f3dc429fcdf265684054a4b963de9113b84
nzsm <53304247+nzsm@users.noreply.github.com>
2021-02-03 13:11:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -36,4 +36,4 @@ sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES 
 #sed -i '$a src-git hwfuture https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' feeds.conf.default
 sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-passwall' feeds.conf.default
+svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-passwall
