0fa1a37fa8e04033d2089c76e7d418765d2b1c74
nzsm <53304247+nzsm@users.noreply.github.com>
2021-07-15 19:06:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -64,8 +64,9 @@ sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feed
 #sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages' feeds.conf.default
 
 
+svn co https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/luci-app-passwall
 #svn co https://github.com/kenzok8/openwrt-packages/branches/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/small-5/luci-app-adblock-plus/trunk/luci-app-adblock-plus package/luci-app-adblock-plus
+
 
 
