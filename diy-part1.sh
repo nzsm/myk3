58e9750572b6b5d0304c92360bdf1a293289f76d
nzsm <53304247+nzsm@users.noreply.github.com>
2021-08-03 00:43:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -44,7 +44,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='nzsm'' package/
 sed -i "s/OpenWrt /nzsm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
@@ -56,24 +56,24 @@ sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES 
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git hwfuture https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' feeds.conf.default
-#sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
-#sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '$a src-git small-5 https://github.com/small-5/luci-app-adblock-plus' feeds.conf.default
-#sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git hwfuture https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' feeds.conf.default
+# sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
+# sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git small-5 https://github.com/small-5/luci-app-adblock-plus' feeds.conf.default
+# sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages' feeds.conf.default
 
 
 svn co https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/luci-app-passwall
-#svn co https://github.com/kenzok8/openwrt-packages/branches/luci-app-passwall package/luci-app-passwall
+# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/luci-app-passwall
+# svn co https://github.com/kenzok8/openwrt-packages/branches/luci-app-passwall package/luci-app-passwall
 
 
-#echo '添加passwall和bypass软件源'
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-#cat feeds.conf.default
-#echo '====================Add feed source OK!===================='
+# echo '添加passwall和bypass软件源'
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# cat feeds.conf.default
+# echo '====================Add feed source OK!===================='
 
 echo '添加jerrykuku的argon-mod主题'
 rm -rf package/lean/luci-theme-argon  
@@ -92,18 +92,18 @@ git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screen
 cat package/lean/k3screenctrl/Makefile |grep DEPENDS
 echo '====================Add k3screen Drive OK!===================='
 
-#echo '移除bcm53xx中的其他机型'
-#sed -i '421,453d' target/linux/bcm53xx/image/Makefile
-#sed -i '140,412d' target/linux/bcm53xx/image/Makefile
-#sed -i 's/k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
-#cat target/linux/bcm53xx/image/Makefile |grep DEVICE_PACKAGES
-#echo '====================Remove other devices of bcm53xx!===================='
+# echo '移除bcm53xx中的其他机型'
+# sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+# sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+# sed -i 's/k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+# cat target/linux/bcm53xx/image/Makefile |grep DEVICE_PACKAGES
+# echo '====================Remove other devices of bcm53xx!===================='
 
 echo '替换K3的无线驱动为asus-dhd24'
 wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.asus-dhd24 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
-#echo '替换K3的无线驱动为ac86u'
-#wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.ac88u -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
-#echo '替换K3的无线驱动为69027'
-#wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.69027 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+# echo '替换K3的无线驱动为ac86u'
+# wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.ac88u -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+# echo '替换K3的无线驱动为69027'
+# wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.69027 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 echo '====================Replace k3wireless firmware OK!===================='
 © 2021 GitHub, Inc.
