641fbf54bbc4dc92d1465cec32001f16229718ca
nzsm <53304247+nzsm@users.noreply.github.com>
2021-08-13 12:53:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+./scripts/feeds update nas
+./scripts/feeds install -a -p nas
