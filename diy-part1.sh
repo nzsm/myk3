0456ef69519c9c531345f3ca6373ff93e948d651
nzsm <53304247+nzsm@users.noreply.github.com>
2022-05-16 12:19:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -41,7 +41,7 @@ sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generat
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='nzsm'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /nzsm build $(TZ=UTC-8 date "+%Y.%m.%d""cut -c1-2 /sys/class/thermal/thermal_zone0/temp") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /nzsm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g""cut -c1-2 /sys/class/thermal/thermal_zone0/temp" package/lean/default-settings/files/zzz-default-settings
 # sed -i "s/OpenWrt /nzsm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g"
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 
